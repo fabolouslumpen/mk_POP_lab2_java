ede25a30cf1c84223c83d7561fd3dea9496c247a
Maksym Kozhushnyi <146103167+fabolouslumpen@users.noreply.github.com>
2024-11-13 20:13:04 +0200

    Update ArrayFill.java

--- ArrayFill.java
+++ ArrayFill.java
@@ -2,7 +2,7 @@ public class ArrayFill {
     public int[] fillArray(int length) {
         int[] array = new int[length];
         for (int i = 0; i < length; i++) {
-            array[i] = (int) (Math.random() * 2001) - 1000;
+            array[i] = (int) (Math.random() * length) - 1;
         }
         return array;
     }
